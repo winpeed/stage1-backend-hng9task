37fa790293e6a8f7b66b35b028a82c38905a19b0
Praise Obende <davidobende@gmail.com>
2022-11-03 12:02:20 +0100

    Change all enum data type values

--- pages/api/stage2.ts
+++ pages/api/stage2.ts
@@ -1,25 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 enum EnumType {
-  addition,
-  subtraction,
-  multiplication,
+  addition = 0,
+  subtraction = 1,
+  multiplication = 2,
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<{
     slackUsername: string;
-    operation_type: string;
+    operation_type: EnumType;
     result: number;
   }>
 ) {
   const calculateResult = (x: number, y: number) => {
-    return req.body.operation_type == EnumType[0]
+    return req.body.operation_type == EnumType.addition
       ? x + y
-      : req.body.operation_type == EnumType[1]
+      : req.body.operation_type == EnumType.subtraction
       ? x - y
-      : req.body.operation_type == EnumType[2]
+      : req.body.operation_type == EnumType.multiplication
       ? x * y
       : 0;
   };
@@ -29,7 +29,7 @@ export default function handler(
     console.log("result2", calculateResult(10, 20));
     res.status(200).json({
       slackUsername: "PraiseObende",
-      operation_type: EnumType[0] || EnumType[1] || EnumType[2],
+      operation_type: EnumType.multiplication,
       result: calculateResult(req.body.x, req.body.y),
     });
   }
